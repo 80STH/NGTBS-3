e3d54be20fc971a7e261b73eb27cec7cb3062e18
Your Name <you@example.com>
2026-05-24 16:52:00 +0600

    ???

--- conf.lua
+++ conf.lua
@@ -1,8 +1,24 @@
 function love.conf(t)
-    t.title = "NGTBS-2"
+    t.title = "Hex Grid Tactics Game"
+    t.author = "Your Name"
     t.version = "11.4"
+    
     t.window.width = 1600
     t.window.height = 1200
     t.window.resizable = true
+    
+    -- Включаем доступ к файловой системе
+    t.identity = "HexTacticsGame"  -- Создает папку для сохранений
+    
+    -- Модули
+    t.modules.audio = true
+    t.modules.graphics = true
+    t.modules.keyboard = true
+    t.modules.mouse = true
+    t.modules.timer = true
+    t.modules.filesystem = true  -- Обязательно для сохранений
 
+
+
+    t.console = true  -- Включает консоль Windows
 end
